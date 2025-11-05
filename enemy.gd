8c9d5a251deb2e74b6e751331ad2e4ca3b828dc8
Hampus The Man <63263777+hampusmagicman@users.noreply.github.com>
2025-11-05 17:50:04 +0100

    Add Enemy-superclass. Not implemented yet

---
+++ enemy.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+# This script is for the enemy class
+# It defines  standard behaviour a
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
