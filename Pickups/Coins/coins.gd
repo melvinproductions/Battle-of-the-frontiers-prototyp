380f7fd47f18e93edb0749508fc2707ab8b65497
Hampus The Man <63263777+hampusmagicman@users.noreply.github.com>
2025-11-08 18:28:08 +0100

    Add Pickup base class, create non-functioning xp

--- Pickups/Coins/coins.gd
+++ Pickups/Coins/coins.gd
@@ -1,8 +1,10 @@
-extends Area2D
+extends Pickup
+class_name Coin
 @export var amount = 0 # placeholder
 
 func _ready():
 	add_to_group("coins")
 
-func picked_up():
+func picked_up(player: Player):
+	player.add_coins(amount)
 	queue_free()
